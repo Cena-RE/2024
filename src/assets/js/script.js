5b756d5a78efb116c10906bcc5fa7265292a1687
re <cena@re.com>
2024-12-05 18:33:00 +0100

    Preliminary version

---
+++ src/assets/js/script.js
@@ -0,0 +1,11 @@
+// JavaScript to handle hover and change the image
+const attendeeItems = document.querySelectorAll('.attendee-item');
+const imageFrame = document.getElementById('attendee-image');
+
+attendeeItems.forEach(item => {
+    item.addEventListener('mouseenter', () => {
+        // Change the image when hovering over an attendee item
+        const imageName = item.getAttribute('data-image');
+        imageFrame.src = `./assets/images/${imageName}`;
+    });
+});
